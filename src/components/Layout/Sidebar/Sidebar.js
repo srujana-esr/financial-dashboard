3823165112017a3ab90a204cfc99949e30b19bbe
Srujana Reddy E <60856611+srujanareddyE@users.noreply.github.com>
2024-11-25 00:27:05 -0600

    added menu and close icons on sprites.svg

--- src/components/Layout/Sidebar/Sidebar.js
+++ src/components/Layout/Sidebar/Sidebar.js
@@ -27,14 +27,14 @@ const Sidebar = ({ isOpen, toggleSidebar }) => {
           <h1 className="text-lg font-bold" style={{ fontSize: '1.563rem' }}>
             Soar Task
           </h1>
-        </div>
         <button
           onClick={toggleSidebar}
-          className="text-gray-500 hover:text-gray-700 focus:outline-none sm:hidden"
+          className="sm:hidden"
           aria-label="Close Sidebar"
         >
           <Icon name="icon-close" size={24} />
         </button>
+        </div>
       </div>
 
       <nav className="flex-grow px-6 py-6">
